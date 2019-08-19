a8f8aba7b592d65fb6b9b7230aef31c745bc70e7
Mithul <mithulmathi@gmail.com>
2019-08-19 18:45:19 -0500

    Added Enemy death animation. #13 #14

--- Enemy.gd
+++ Enemy.gd
@@ -8,21 +8,28 @@ export var health = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	$Healthbar.value = 20
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	if randf() < 0.01:
-		$AnimatedSprite.play("Blink")
+		if $AnimatedSprite.animation != 'Death':
+			$AnimatedSprite.play("Blink")
 	else:
 		if $AnimatedSprite.animation == 'Blink' and $AnimatedSprite.frames.get_frame_count("Blink") == $AnimatedSprite.frame + 1:
 			$AnimatedSprite.play("Idle")
+		elif $AnimatedSprite.animation == 'Death' and $AnimatedSprite.frames.get_frame_count("Death") == $AnimatedSprite.frame + 1:
+			queue_free()
 
 func die():
-	queue_free()
+	if $AnimatedSprite.animation != 'Death':
+		$AnimatedSprite.play("Death")
 
 func incur_damage(damage):
+	if health < 0:
+		return false
 	health -= damage
+	$Healthbar.value = health/5
 	if health < 0:
 		die()
 	
