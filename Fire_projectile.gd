217923a75a4a481b6ef36d7c91efa1d0ad4941bd
Mithul <mithulmathi@gmail.com>
2019-08-19 13:05:36 -0500

    Fix #1. Fireball direction

--- Fire_projectile.gd
+++ Fire_projectile.gd
@@ -5,12 +5,25 @@ extends Node2D
 # var b = "text"
 export var speed = 40
 # Called when the node enters the scene tree for the first time.
+
+func init(direction):
+	print("INIT", direction)
+	if direction == constants.UP:
+		set_rotation(deg2rad(-90))
+	elif direction == constants.DOWN:
+		set_rotation(deg2rad(90))
+	elif direction == constants.LEFT:
+		set_rotation(deg2rad(180))
+	elif direction == constants.RIGHT:
+		set_rotation(deg2rad(0))
+
 func _ready():
-	pass # Replace with function body.
+	$flight_particles.local_coords = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position += Vector2(1,0)*speed*delta
+	
+	translate((Vector2(1,0)*speed*delta).rotated(rotation))
 
 
 func _on_lifetime_timeout():
